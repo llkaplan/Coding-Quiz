cb854844cc0d0ec7b41686a997fc93625245fd2b
llkaplan <35112665+llkaplan@users.noreply.github.com>
2019-10-15 12:40:52 -0700

    Second update

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-window.onload=function(){
-
+window.onload = function () {
 
     //global vars
     var score = 0;
@@ -7,64 +6,68 @@ window.onload=function(){
     var firstPage = document.getElementById('quizContent');
     var questionPage = document.getElementById('quiz');
     var nextButton = document.getElementById('next');
-
+    var iter = 0;
+    var curAns = '';
 
     // storing all my questions
 
     var myQuestions = [
         {
             question: "What keywords do you use to declare variables",
-            answers: ["Var and Name", "Variable and Let", "Var and Let"
-        ],
+            answers: ["Var and Name", "Variable and Let", "Var and Let"],
             correctAnswer: 'Var and Let'
         },
         {
             question: "Which of the following isn't a data type?",
-            answers: ["String", "Group", "Boolean", "Array", "Object"
-        ],
+            answers: ["String", "Group", "Boolean", "Array", "Object"],
             correctAnswer: 'b'
         },
         {
             question: "What is the proper syntax for an click event?",
-            answers: ["addEventListener(\"click\", myScript)", "click.function()", "addEventListener(click)"
-        ],
+            answers: ["addEventListener(\"click\", myScript)", "click.function()", "addEventListener(click)"],
             correctAnswer: 'a'
         }
     ];
 
-    startBtn.addEventListener("click", function(){
-        firstPage.style.display="none";
-        questionPage.style.display="block";
+
+    startBtn.addEventListener("click", function () {
+        firstPage.style.display = "none";
+        questionPage.style.display = "block";
         var questionText = document.createElement("h3");
-        questionText.innerHTML = myQuestions[0].question;
+        questionText.innerHTML = myQuestions[iter].question;
         questionPage.appendChild(questionText);
         questionPage.insertBefore(questionText, null);
         questionPage.insertBefore(questionText, questionPage.childNodes[0] || null);
 
-        for (i = 0; i < myQuestions[0].answers.length; i++){
+        for (i = 0; i < myQuestions[iter].answers.length; i++) {
             var answerButton = document.createElement("button");
-            answerButton.innerHTML = myQuestions[0].answers[i];
+            answerButton.innerHTML = myQuestions[iter].answers[i];
             answerButton.classList.add("btn", 'btnlight', "answerBtn");
             questionPage.appendChild(answerButton);
             questionPage.insertBefore(answerButton, null);
-            questionPage.insertBefore(answerButton, questionPage.childNodes[1] || null);
+            questionPage.insertBefore(answerButton, questionPage.childNodes[1] || null); 
         };
 
-        answerButton.addEventListener("click", function(){
-            if(answerButton = myQuestions[0].correctAnswer){
+        answerButton.addEventListener("click", function () {
+           curAns = answerButton; 
+        });
+
+        nextButton.addEventListener("click", function () {
+            if (curAns = myQuestions[iter].correctAnswer) {  
                 score++;
                 console.log(score);
             }
-        });
+            if (iter < (myQuestions.length - 1)) {
+                iter++;
+                console.log(iter);
 
-        for (i = 0; i < myQuestions.length; i++){
-            myQuestions[+1]
-        }
-
-        nextButton.addEventListener("click", function(){
+            }
+            else{        
+                firstPage.style.display = "none";
+                questionPage.style.display = "none";
+                lastPage.style.display = "block";
+             }
+        })
     });
 
-
 }
-
-
